ebe453b6094c21324e552cda79cf3fde13424fe1
Jadarma <dancristiancimpianu@gmail.com>
2023-11-29 19:18:53 +0200

    Update Kotlin to 1.9.21.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
 import org.gradle.kotlin.dsl.KotlinClosure2
 
 plugins {
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "1.9.21"
 }
 
 kotlin {
